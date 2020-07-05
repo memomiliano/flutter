e5f7ccee1d13095bdbfab33f3f3efb912d97a1ba
Maximiliano Ruiz <maximiliano.ruiz@bci.cl>
2020-07-05 11:55:46 -0400

    Final code to myCard Flutter Project

--- my_card_flutter/lib/main.dart
+++ my_card_flutter/lib/main.dart
@@ -11,57 +11,68 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
-                child: Text(
-                  'Row 1',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/profile.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow,
-                    child: Text(
-                      'Container 1',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.blueGrey, fontWeight: FontWeight.bold),
+              Text(
+                'Maximiliano Ruiz',
+                style: TextStyle(
+                    fontFamily: 'MontserratSubrayada',
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'MOBILE DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.w200),
+              ),
+              Divider(
+                height: 10.0,
+                color: Colors.teal.shade100,
+                indent: 140.0,
+                endIndent: 140.0,
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
                     ),
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.green,
-                    child: Text(
-                      'Container 2',
-                      textAlign: TextAlign.center,
+                    title: Text(
+                      '+56 951088357',
                       style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro'),
                     ),
-                  ),
-                ],
-              ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
-                child: Text(
-                  'Row 3',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-              ),
+                  )),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'maximiliano.ruiz.g@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  ))
             ],
           ),
         ),
